0a0744a5b7519edf378519165b75b721c9725b59
Chau Denis <chaudenisbreiz@yahoo.fr>
2025-11-12 10:01:22 +0100

    front: - replace App.tsx entry point with index.js for Expo root registration - remove manual entryPoint from app.json to let Expo use default index.js entry - update package.json main entry from App.tsx to index.js

---
+++ temp_backup/env.js
@@ -0,0 +1,9 @@
+// export const API_BASE_URL = process.env.REACT_NATIVE_API_URL || 'http://localhost:5000'; // gateway or content service
+export const API_BASE_URL = process.env.REACT_NATIVE_API_URL || 'http://192.168.1.44:5000'; // remplace 192.168.1.45 par ton IP machine si besoin
+
+export const ADMOB_TEST_REWARDED_ID = 'ca-app-pub-3940256099942544/5224354917'; // expo test id
+export const ADMOB_TEST_BANNER_ID = 'ca-app-pub-3940256099942544/6300978111';
+export const APP_PACKAGE = 'com.bowo.app';
+
+
+
