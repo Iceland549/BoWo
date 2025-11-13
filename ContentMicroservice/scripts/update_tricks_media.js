638c69aa749854248b2651107cf23ac1f4fb45e8
Chau Denis <chaudenisbreiz@yahoo.fr>
2025-11-13 10:16:21 +0100

    back-content: - enable static media (wwwroot) and update Dockerfile - add update_tricks_media.js for local media URLs

---
+++ ContentMicroservice/scripts/update_tricks_media.js
@@ -0,0 +1,31 @@
+﻿use bowo - content - db;
+
+db.tricks.updateOne(
+    { _id: "ollie" },
+    {
+        $set: {
+            "images": [
+                "http://192.168.1.44:5002/tricks/ollie/ollie_1.jpg",
+                "http://192.168.1.44:5002/tricks/ollie/ollie_2.jpg",
+                "http://192.168.1.44:5002/tricks/ollie/ollie_3.jpg"
+            ],
+            "videos.amateurUrl": "",
+            "videos.proUrl": ""
+        }
+    }
+);
+
+db.tricks.updateOne(
+    { _id: "popshoveit" },
+    {
+        $set: {
+            "images": [
+                "http://192.168.1.44:5002/tricks/popshoveit/popshoveit_1.jpg",
+                "http://192.168.1.44:5002/tricks/popshoveit/popshoveit_2.jpg",
+                "http://192.168.1.44:5002/tricks/popshoveit/popshoveit_3.jpg"
+            ],
+            "videos.amateurUrl": "http://192.168.1.44:5002/tricks/popshoveit/CRITERIUM%202019.mp4",
+            "videos.proUrl": ""
+        }
+    }
+);
