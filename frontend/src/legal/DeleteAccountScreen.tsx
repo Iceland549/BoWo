c2a6321a8650f645298e08054381748c360f67ab
Chau Denis <chaudenisbreiz@yahoo.fr>
2025-12-02 13:43:44 +0100

    fix-frontend: erase Alert from import

--- frontend/src/legal/DeleteAccountScreen.tsx
+++ frontend/src/legal/DeleteAccountScreen.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, Text, StyleSheet, TouchableOpacity, Alert } from "react-native";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import { NativeStackNavigationProp } from "@react-navigation/native-stack";
 import { RootStackParamList } from "../navigation/types";
